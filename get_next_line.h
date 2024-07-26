0c9ba6e43756cbe4aa3da4741fbbbc3a8f9dbd09
BlanckSpeed <inrodrigoant@gmail.com>
2024-07-26 17:55:52 +0200

    feat: correction of names, leaks and functions named with gnl

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: rodrigo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 17:25:36 by rodrigo           #+#    #+#             */
-/*   Updated: 2024/07/26 17:25:40 by rodrigo          ###   ########.fr       */
+/*   Updated: 2024/07/26 17:48:34 by rodrigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 512
 # endif
+
 # if BUFFER_SIZE > 125000 || BUFFER_SIZE < 1
 #  undef BUFFER_SIZE
 #  define BUFFER_SIZE 512
@@ -31,7 +33,7 @@ char	*init_line(char *stash, int *eol_loc);
 size_t	locate_eol(char *line);
 
 size_t	ft_strlen(const char *s);
-void	ft_bzero(void *s, size_t n);
+void	gnl_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_strjoin_gnl(char *s1, char *s2, int *eol_loc);
 void	ft_strlcpy_gnl(char *dst, const char *src, size_t dstsize);
