83a2506858594174a54d68b5c88b28550505159b
BlanckSpeed <inrodrigoant@gmail.com>
2024-07-26 17:27:56 +0200

    feat: subido todo

---
+++ get_next_line.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrigo <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/26 17:25:16 by rodrigo           #+#    #+#             */
+/*   Updated: 2024/07/26 17:25:19 by rodrigo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+char	*init_line(char *buffer, int *eol_loc)
+{
+	size_t	len;
+	char	*line;
+
+	len = 0;
+	while (buffer[len] && buffer[len] != '\n')
+		len++;
+	len++;
+	line = malloc(sizeof(char) * (len + 1));
+	if (!line)
+		return (NIL);
+	ft_memcpy(line, buffer, len);
+	line[len] = '\0';
+	if (len > 0 && line[len - 1] == '\n')
+		*eol_loc = len - 1;
+	return (line);
+}
+
+size_t	locate_eol(char *line)
+{
+	size_t	i;
+
+	i = 0;
+	if (!line)
+		return (-1);
+	while (i < BUFFER_SIZE)
+	{
+		if (line[i] == '\n' || line[i] == '\0')
+			return (i + 1);
+		i++;
+	}
+	return (i);
+}
+
+char	*extract_line(char *line, char *buffer, int *eol_loc, int fd)
+{
+	char	temp_buffer[BUFFER_SIZE + 1];
+	ssize_t	read_check;
+	size_t	line_size;
+
+	while (*eol_loc == -1)
+	{
+		gnl_bzero(temp_buffer, (BUFFER_SIZE + 1));
+		read_check = read(fd, temp_buffer, BUFFER_SIZE);
+		if (read_check == -1)
+		{
+			free(line);
+			gnl_bzero(buffer, (BUFFER_SIZE + 1));
+			return (NIL);
+		}
+		line_size = locate_eol(temp_buffer);
+		ft_strlcpy_gnl(buffer, &temp_buffer[line_size], (BUFFER_SIZE + 1));
+		temp_buffer[line_size] = '\0';
+		line = ft_strjoin_gnl(line, temp_buffer, eol_loc);
+		if (read_check == 0)
+		{
+			gnl_bzero(buffer, BUFFER_SIZE + 1);
+			break ;
+		}
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*line;
+	int			eol_loc;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NIL);
+	eol_loc = -1;
+	line = init_line(buffer, &eol_loc);
+	if (!line)
+		return (NIL);
+	ft_strlcpy_gnl(buffer, &buffer[eol_loc + 1], BUFFER_SIZE + 1);
+	line = extract_line(line, buffer, &eol_loc, fd);
+	if (!line || line[0] == '\0')
+	{
+		free(line);
+		return (NIL);
+	}
+	return (line);
+}
+/*
+int	main(void)
+{
+	int		fd;
+	int		cont;
+	char	*str;
+	char	*path;
+
+	cont = 0;
+	path = "/home/rodrigo/Proyectos42/get_next_line/miFichero";
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error opening file");
+		return (1);
+	}
+	while (1)
+	{
+		str = get_next_line(fd);
+		if (str == NIL)
+			break ;
+		cont++;
+		printf("[%d]:%s\n", cont, str);
+		free(str);
+		str = NIL;
+	}
+	close(fd);
+	return (0);
+}*/
---
+++ get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrigo <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/26 17:25:36 by rodrigo           #+#    #+#             */
+/*   Updated: 2024/07/26 17:25:40 by rodrigo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 512
+# endif
+# if BUFFER_SIZE > 125000 || BUFFER_SIZE < 1
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 512
+# endif
+
+# define NIL 0
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*extract_line(char *line, char *stash, int *eol_loc, int fd);
+char	*init_line(char *stash, int *eol_loc);
+size_t	locate_eol(char *line);
+
+size_t	ft_strlen(const char *s);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strjoin_gnl(char *s1, char *s2, int *eol_loc);
+void	ft_strlcpy_gnl(char *dst, const char *src, size_t dstsize);
+#endif
